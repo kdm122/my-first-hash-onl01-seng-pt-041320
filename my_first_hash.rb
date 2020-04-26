e9ee7455f910b6658ed2b7d82162c33ec726e11e
Kyle McLean <kyledmclean@gmail.com>
2020-04-26 01:26:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+kyle_hash = {}
   
 end
 
@@ -8,7 +8,12 @@ end
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings {
+        "whale bone corsets" => 5,
+        "procelain vases" => 2,
+        "oil paintings" => 3
+        }
+  the_manifest = }
 
 
 end
